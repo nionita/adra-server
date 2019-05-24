9458f2374589fc6a969fff25936bc943c239ccca
Nicu Ionita <nicu@ionita.at>
2019-05-24 17:28:21 +0200

    Make use of promises in cardreader functions

--- cardreader.js
+++ cardreader.js
@@ -1,3 +1,4 @@
+const util = require('util')
 const keys = require('./config/keys')
 const state = require('./state')
 
@@ -15,19 +16,9 @@ const cmd_get_UID = Buffer.from([0xFF, 0xCA, 0x00, 0x00, 0x04])
 const cmd_get_ATS = Buffer.from([0xFF, 0xCA, 0x01, 0x00, 0x04]) // not supported on MIFARE 1k
 // const cmd_load_key = Buffer.from('FF82000006000000000000', 'hex') // we use only one key (location 0); key must be copied to the last 6 bytes of the buffer
 const cmd_load_key = Buffer.from('FF82000006FFFFFFFFFFFF', 'hex') // we use only one key (location 0); key must be copied to the last 6 bytes of the buffer
-const cmd_auth_a = Buffer.from('FF860000050100006000', 'hex') // authenticate block (position 7) with key type A, key location 0
-const cmd_auth_b = Buffer.from('FF860000050100006100', 'hex') // authenticate block (position 7) with key type B, key location 0
+const cmd_auth = Buffer.from('FF860000050100006000', 'hex') // authenticate block (position 7) with key type A, key location 0
 const cmd_read = Buffer.from('FFB0000010', 'hex') // read 16 bytes from block (position 3)
 // const cmd_write = Buffer.from('FFD600001000000000000000000000000000000000', 'hex') // write 16 bytes in block (position 3); data must be copied to the last 16 bytes of the buffer
-
-// We must write our block number in all commands that involve a block number
-function set_block(block_number) {
-  cmd_auth_a[7] = block_number
-  cmd_auth_b[7] = block_number
-  cmd_read[3] = block_number
-}
-
-set_block(our_block_number)
 
 // We accept only MIFARE 1k cards for now
 function is_mifare_1k(atr) {
@@ -35,15 +26,47 @@ function is_mifare_1k(atr) {
 }
 
 // APDU with the PICC: send command, receive response
-function APDU(reader, protocol, cmd, next) {
-  reader.transmit(cmd, 256, protocol, function(err, data) {
-    if (err) {
-      console.log('Reader ' + reader.name + ': transmit error:', err)
-    } else {
-      console.log('Reader ' + reader.name + ': received data:', data)
-      next(data)
-    }
-  });
+function APDU(reader, protocol, cmd, debug=false) {
+  return new Promise((resolve, reject) => {
+    reader.transmit(cmd, 256, protocol, function(err, data) {
+      if (err) {
+        console.log('Reader ' + reader.name + ': transmit error:', err)
+        reject(err)
+      } else {
+        if (debug) {
+          console.log('Reader ' + reader.name + ': received data:', data)
+        }
+        resolve(data)
+      }
+    })
+  })
+}
+
+async function APDU_auth(reader, protocol, block, keytype='A', keyloc=0, debug=false) {
+  const cmd = Buffer.from(cmd_auth)
+  if (keytype === 'B') {
+    cmd[8] = 0x61
+  } else if (keytype !== 'A') {
+    throw('Wrong key type ' + keytype)
+  }
+  if (keyloc === 1) {
+    cmd[9] = 0x01
+  } else if (keyloc !== 0) {
+    throw('Wrong key location ' + keyloc)
+  }
+  if (block >= 64) {
+    throw('Block too big ' + block)
+  }
+  cmd[7] = block
+  try {
+    const data = await APDU(reader, protocol, cmd, debug)
+    // Check the success of the operation
+    const err = APDU_error(data)
+    return err === APDU_SUCCESS
+  }
+  catch (e) {
+    return false
+  }
 }
 
 // APDU success/errors
@@ -74,39 +97,49 @@ function APDU_payload(data) {
   return payload
 }
 
-function make_auth_key() {
-  // Check the secret
-  if (!keys.adraSecret || keys.adraSecret.length < 12) {
-    console.error('ADRA secret not correct!')
-    process.exit(1)
+function make_auth_key(key) {
+  // Check the key
+  if (!key || key.length !== 12) {
+    throw('Key not correct')
   }
   try {
-    const buf_key = Buffer.from(keys.adraSecret, 'hex')
+    const buf_key = Buffer.from(key, 'hex')
     buf_key.copy(cmd_load_key, 5, 0, 6)
   }
   catch (e) {
-    console.error('Cannot create authentication key', e)
-    process.exit(1)
+    throw('Cannot create authentication key:', e)
   }
 }
 
 // Load the auth key - once per server session
-function load_auth_key(reader, protocol, next) {
-  if (!state.readerHasAuthKey()) {
-    console.log('Reader ' + reader.name + ': load authentication key')
-    APDU(reader, protocol, cmd_load_key, function(resp) {
-      const err = APDU_error(resp)
-      if (err === APDU_SUCCESS) {
-        state.readerSentAuthKey()
-        console.log('Reader ' + reader.name + ': authentication key loaded')
-        next()
-      } else {
-        console.log('Reader ' + reader.name + ': APDU error:', APDU_ERR_MESSAGES[err])
+function load_auth_key(reader, protocol, debug=false) {
+  return new Promise((resolve, reject) => {
+    if (state.readerHasAuthKey()) {
+      resolve()
+    } else {
+      if (debug) {
+        console.log('Reader ' + reader.name + ': load authentication key')
       }
-    })
-  } else {
-    next()
-  }
+      APDU(reader, protocol, cmd_load_key, debug)
+        .then(resp => {
+          const err = APDU_error(resp)
+          if (err === APDU_SUCCESS) {
+            state.readerSentAuthKey()
+            if (debug) {
+              console.log('Reader ' + reader.name + ': authentication key loaded')
+            }
+            resolve()
+          } else {
+            console.log('Reader ' + reader.name + ': APDU error:', APDU_ERR_MESSAGES[err])
+            reject()
+          }
+        })
+        .catch((err) => {
+          console.log('Reader ' + reader.name + ': APDU exception:', err)
+          reject()
+        })
+    }
+  })
 }
 
 function card_inserted(reader) {
@@ -114,24 +147,27 @@ function card_inserted(reader) {
   // Card connect options
   const connect_options = { share_mode: reader.SCARD_SHARE_SHARED }
   // const connect_options = { share_mode: reader.SCARD_SHARE_DIRECT }
-  reader.connect(connect_options, function(err, protocol) {
+  reader.connect(connect_options, async function(err, protocol) {
     if (err) {
       console.log('Reader ' + reader.name + ': connect error:', err)
     } else {
       console.log('Reader ' + reader.name + ': protocol:', protocol)
-      APDU(reader, protocol, cmd_get_UID, function(data) {
+      try {
+        const data = await APDU(reader, protocol, cmd_get_UID)
         // Check the success of the operation
         const err = APDU_error(data)
         if (err === APDU_SUCCESS) {
           const cuid = APDU_payload(data)
-          load_auth_key(reader, protocol, function() {
-            state.readerConnected(reader, protocol, cuid)
-            console.log('Card UID:', cuid)
-          })
+          await load_auth_key(reader, protocol)
+          state.readerConnected(reader, protocol, cuid)
+          console.log('Card UID:', cuid)
         } else {
           console.log('Reader ' + reader.name + ': APDU error:', APDU_ERR_MESSAGES[err])
         }
-      })
+      }
+      catch (e) {
+        console.log('Reader ' + reader.name + ': connect error:', e)
+      }
     }
   })
 }
@@ -186,6 +222,26 @@ pcsc.on('error', function(err) {
 })
 
 // Interface to access the card: read and write the block 0 of the current card (if any)
+function read_block() {
+  const { reader, protocol } = state.getROState()
+  if (!reader) {
+    return { message: 'Reader is not connected or no card present' }
+  }
+  return new Promise(function(resolve, reject) {
+    console.log('Reader ' + reader.name + ': read block 0')
+    APDU(reader, protocol, cmd_read, function(data) {
+      const err = APDU_error(data)
+      if (err === APDU_SUCCESS) {
+        const block_data = APDU_payload(data)
+        resolve({ data: block_data })
+      } else {
+        console.log('Reader ' + reader.name + ': APDU error:', APDU_ERR_MESSAGES[err])
+        resolve({ message: 'Read - ' + APDU_ERR_MESSAGES[err] })
+      }
+    })
+  })
+}
+
 function read_block_auth() {
   return new Promise(function(resolve, reject) {
     const { reader, protocol } = state.getROState()
@@ -216,20 +272,20 @@ function read_block_auth() {
   })
 }
 
-function APDU_promise(reader, protocol, cmd, block) {
-  return new Promise(function(resolve, reject) {
-    set_block(block)
-    APDU(reader, protocol, cmd, function(data) {
-      // Check the success of the operation
-      const err = APDU_error(data)
-      if (err === APDU_SUCCESS) {
-        resolve(true)
-      } else {
-        resolve(false)
-      }
-    })
-  })
-}
+// function APDU_promise(reader, protocol, cmd, block) {
+//   return new Promise(function(resolve, reject) {
+//     set_block(block)
+//     APDU(reader, protocol, cmd, function(data) {
+//       // Check the success of the operation
+//       const err = APDU_error(data)
+//       if (err === APDU_SUCCESS) {
+//         resolve(true)
+//       } else {
+//         resolve(false)
+//       }
+//     })
+//   })
+// }
 
 function default_sector_auth() {
   return new Promise(async function(resolve, reject) {
@@ -238,47 +294,77 @@ function default_sector_auth() {
       resolve({ message: 'Reader is not connected or no card present' })
     }
     console.log('Find sectors with default auth')
-    const key_type = [
-      { type: 'A', cmd: cmd_auth_a, sectors: [] },
-      { type: 'B', cmd: cmd_auth_b, sectors: [] },
-    ]
-    for (kt of key_type) {
-      console.log('Key type', kt.type)
-      for (var sect = 0; sect < 16; sect++) {
-        const block = sect * 4
-        const ok = await APDU_promise(reader, protocol, kt.cmd, block)
+    const sectors = new Set()
+    for (var sect = 0; sect < 16; sect++) {
+      const block = sect * 4
+      var ok = false
+      try {
+        ok = await APDU_auth(reader, protocol, block)
         console.log('Sector', sect, 'block', block, ':', ok)
-        if (ok) {
-          kt.sectors.append(sect)
-        }
+      }
+      catch (e) {
+        console.log('APDU_auth error:', e)
+      }
+      if (ok) {
+        sectors.add(sect)
       }
     }
-    resolve(key_type.map(kt => kt.sectors))
+    resolve(sectors)
   })
 }
 
-function read_block() {
-  const { reader, protocol } = state.getROState()
-  if (!reader) {
-    return { message: 'Reader is not connected or no card present' }
-  }
-  return new Promise(function(resolve, reject) {
-    console.log('Reader ' + reader.name + ': read block 0')
-    APDU(reader, protocol, cmd_read, function(data) {
-      const err = APDU_error(data)
-      if (err === APDU_SUCCESS) {
-        const block_data = APDU_payload(data)
-        resolve({ data: block_data })
-      } else {
-        console.log('Reader ' + reader.name + ': APDU error:', APDU_ERR_MESSAGES[err])
-        resolve({ message: 'Read - ' + APDU_ERR_MESSAGES[err] })
+// Search a correct auth key from a list of default MIFARE keys
+function scan_sector_auth() {
+  return new Promise(async function(resolve, reject) {
+    const { reader, protocol } = state.getROState()
+    if (!reader) {
+      resolve({ message: 'Reader is not connected or no card present' })
+    }
+    console.log('Find sectors with default mifare auth keys')
+    const dmkeys = require('./mifare_keys')
+    const found = {}
+    for (key of dmkeys) {
+      var key_ok = true
+      try {
+        make_auth_key(key)
       }
-    })
+      catch (e) {
+        console.log('Key', key, 'error:', e)
+        key_ok = false
+      }
+      if (key_ok) {
+        console.log('Key', key)
+        // Prepare to load the new key
+        state.readerNew()
+        await load_auth_key(reader, protocol)
+        const sectors = []
+        for (var sect = 0; sect < 16; sect++) {
+          // Only the first block from every sector
+          const block = sect * 4 + 3
+          var ok = false
+          try {
+            ok = await APDU_auth(reader, protocol, block, 'B')
+            console.log('Sector', sect, 'block', block, ':', ok)
+          }
+          catch (e) {
+            console.log('APDU_auth error:', e)
+          }
+          if (ok) {
+            sectors.append(sect)
+          }
+        }
+        if (sectors.length > 0) {
+          found['key'] = []
+        }
+      }
+    }
+    resolve(found)
   })
 }
 
 module.exports = {
   // read_block: read_block,
-  // read_block: read_block_auth,
-  read_block: default_sector_auth,
+  read_block: read_block_auth,
+  read_default: default_sector_auth,
+  scan_auth: scan_sector_auth,
 }
--- index.js
+++ index.js
@@ -10,6 +10,20 @@ app.get('/api/read', async (req, res) => {
   res.send(JSON.stringify(block_data))
 })
 
+app.get('/api/readdef', async (req, res) => {
+  console.log('API call: readdef')
+  const scan_result = await cardreader.read_default()
+  console.log('Read result:', scan_result)
+  res.send(JSON.stringify(scan_result))
+})
+
+app.get('/api/scan', async (req, res) => {
+  console.log('API call: scan')
+  const scan_result = await cardreader.scan_auth()
+  console.log('Read result:', scan_result)
+  res.send(JSON.stringify(scan_result))
+})
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Listening on port`, PORT)
